ffb6115b190c60889b50605bb960a5066c6d2b20
William <william@webgoal.com.br>
2016-12-11 12:05:07 -0200

    List Views

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  root :to => 'atividade_usuario#index'
   resources :participante_atividades
   resources :usuarios
+  resources :atividade_usuario
   devise_for :users, :controllers => { :omniauth_callbacks => "omniauth_callbacks" }
   get "atividades_usuarios", to: 'atividade_usuario#atividade_usuario'
-  
+# get "atividade_usuarios", to: 'atividade_usuario#index'
+
+
 end
