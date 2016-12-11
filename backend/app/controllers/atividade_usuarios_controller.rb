37abd3b9ce78273c836402a1e4786b4e52f99bda
Jeff Albuquerque <jeff@webgoal.com.br>
2016-12-11 12:06:45 -0200

    associacao de usuario atividades

---
+++ backend/app/controllers/atividade_usuarios_controller.rb
@@ -0,0 +1,43 @@
+class AtividadeUsuariosController < ApplicationController
+
+	def atividade_usuario
+	end
+
+	def index
+		@atividades_usuario = AtividadeUsuario.new
+	end
+
+
+	def destroy
+		@atividade_usuario.destroy
+		respond_to do |format|
+			format.html { redirect_to usuarios_url, notice: 'Atividade do Usuario foi deletada corretamente.' }
+			format.json { head :no_content }
+		end
+	end
+
+	def create
+ 	# 	atividade_usuario = atividade_params[:atividade_usuario]
+    # @usuario = atividade_usuario[:usuario]
+		# @atividade = atividade_usuario[:atividade]
+		# @atividade_usuario = AtividadeUsuario.new(usuario_id: @usuario, atividade_id: @atividade)
+		AtividadeUsuario.create(atividade_params)
+
+    # respond_to do |format|
+		# 	@atividade_usuario.save
+    #   # if @atividade_usuario.save
+    #   #   # format.html { redirect_to @usuario, notice: 'Atividade do Usuario foi cadastrada corretamente.' }
+    #   #   # format.json { render :show, status: :created, location: @usuario }
+    #   # else
+    #   #   format.html { render :new }
+    #   #   format.json { render json: @usuario.errors, status: :unprocessable_entity }
+    #   # end
+    # end
+  end
+
+	private
+
+	def atividade_params
+		params.require(:atividade_usuario).permit(:usuario_id, :atividade_id)
+	end
+end
--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   resources :participante_atividades
   resources :usuarios
+  resources :atividade_usuarios
   devise_for :users, :controllers => { :omniauth_callbacks => "omniauth_callbacks" }
   get "atividades_usuarios", to: 'atividade_usuario#atividade_usuario'
-  
+
 end
